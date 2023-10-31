43df71dc8f61a22c3ed50414ba92d99a500e6cc4
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-31 11:40:25 +0100

    Update vakken.mjs

--- vakken.mjs
+++ vakken.mjs
@@ -9,16 +9,51 @@ let vakkenDitSemester = {
     },
     WebEssentials: {
         naam: "Web Essentials",
-        docent: "Docent naam 2"
+        docent: "Tom Aertssens"
     },
     NetworkEssentials: {
         naam: "Network Essentials",
-        docent: "Tom Aertssens"
+        docent: "naamDocent"
+    },
+    BusinessCommunication: {
+        naam: "Business Communication TI1",
+        academiejaar: "naamDocent"
+    },
+    BusinessITEssentials: {
+        naam: "Business IT Essentials TI1",
+        academiejaar: "naamDocent"
+    },
+    DataEssentials: {
+        naam: "Data Essentials TI1",
+        academiejaar: "naamDocent"
+    },
+    DesktopOS: {
+        naam: "Desktop OS TI1",
+        academiejaar: "naamDocent"
+    },
+    iTalent1: {
+        naam: "iTalent 1 TI1",
+        academiejaar: "naamDocent"
+    },
+    ProgrammingEssentials1: {
+        naam: "Programming Essentials 1 TI1",
+        academiejaar: "naamDocent"
+    },
+    ProgrammingEssentials2: {
+        naam: "Programming Essentials 2 TI1",
+        academiejaar: "naamDocent"
+    },
+    ProgrammingProject: {
+        naam: "Programming Project TI1",
+        academiejaar: "naamDocent"
+    },
+    SoftwareDesignEssentials: {
+        naam: "Software Design Essentials TI1",
+        academiejaar: "naamDocent"
+    },
+    WebAdvancedTI1: {
+        naam: "Web Advanced TI1",
+        academiejaar: "naamDocent"
     },
 };
 
-
-
-console.log(vakkenDitSemester.ITEssentials.naam);
-
-process.exit();
